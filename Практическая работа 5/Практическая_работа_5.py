59e1eae9df7f91e0b9ca01242efb51093ee0ee9c
Юлия Ванярина <90899361+Julia-Vanyarina@users.noreply.github.com>
2021-12-20 02:07:49 +0300

    2

--- Практическая работа 5/Практическая_работа_5.py
+++ Практическая работа 5/Практическая_работа_5.py
@@ -13,4 +13,10 @@ class Room:
         for i in self.wd:
             new_square -= i.square
         return new_square
-    
+r1 = Room(6, 3, 2.7)
+print(r1.square) #общая площадь квартиры
+r1.addWD(1, 1) #площадь окна
+r1.addWD(1, 1) #площадь окна
+r1.addWD(1, 2) #площадь двери
+print(r1.workSurface())
+
